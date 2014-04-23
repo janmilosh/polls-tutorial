8ec67aba4c8a5303c6ab619e3068747db6204c22
Jan Milosh <janmilosh@gmail.com>
2014-04-22 20:30:26 -0400

    Starts routing skeleton

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def index(request, poll_id):
     return HttpResponse("Hello, world. You're at the poll index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at the poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of the poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
